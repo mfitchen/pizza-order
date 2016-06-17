cc4315f3b4e24634afdf234bc25971478e3d3f6c
Marty Fitchen <mfitchen@gmail.com>
2016-06-17 15:01:17 -0700

    clean up commented out code and adds a nav bar to the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,10 @@
 // Business Logic
+
 // Pizza Constructor
 function Pizza(size) {
   this.pizzasize = size;
   this.pizzaingredients = [];
 };
-
-// function Ingredients(chs, pep, blp, cbn, pna, olv, msh, xch) {
-//   this.addcheese = chs;
-//   this.addpepperoni = pep;
-//   this.addbellpeppers = blp;
-//   this.addcanadianbacon = cbn;
-//   this.addpineapple = pna;
-//   this.addolives = olv;
-//   this.addmushrooms = msh;
-//   this.addxcheese = xch;
-// };
 
 // Price Prototype Method
 Pizza.prototype.price = function() {
@@ -34,42 +24,7 @@ Pizza.prototype.price = function() {
     price += this.pizzaingredients.length;
   }
 
-  // if (this.pizzasize === "Large") {
-  //   price *= 3;
-  // } else if (this.pizzasize === "Medium") {
-  //   price *= 2;
-  // } else if (this.pizzasize === "Small") {
-  //   price *= 1;
-  // } else if (this.addcheese === "true") {
-  //   // this.pizzaingredients.push("Cheese");
-  //   price += 1;
-  // } else if (this.addpepperoni === "true") {
-  //   // this.pizzaingredients.push("Pepperoni");
-  //   price += 1;
-  // } else if (this.addbellpeppers === "true") {
-  //   // this.pizzaingredients.push("Bell Peppers");
-  //   price += 1;
-  // } else if (this.addcanadianbacon === "true") {
-  //   // this.pizzaingredients.push("Canadian Bacon");
-  //   price += 1;
-  // } else if (this.addpineapple === "true") {
-  //   // this.pizzaingredients.push("Pineapple");
-  //   price += 1;
-  // } else if (this.addolives === "true") {
-  //   // this.pizzaingredients.push("Olives");
-  //   price += 1;
-  // } else if (this.addmushrooms === "true") {
-  //   // this.pizzaingredients.push("Mushrooms");
-  //   price += 1;
-  // } else if (this.addxcheese === "true") {
-  //   // this.pizzaingredients.push("Extra Cheese");
-  //   price += 1;
-  // } else {
-  //   price *= 1;
-  // }
-
   return price;
-
 };
 
 // Front-End User Logic
